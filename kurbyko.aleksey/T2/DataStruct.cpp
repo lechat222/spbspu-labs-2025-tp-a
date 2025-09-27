77faad4319540d3375cf54a2c345e628b29db9d1
lechat222 <kurbyko2006@gmail.com>
2025-09-27 23:49:54 +0300

    Adding DataStruct.hpp and DataStruct.cpp

---
+++ kurbyko.aleksey/T2/DataStruct.cpp
@@ -0,0 +1,7 @@
+#include "DataStruct.hpp"
+
+namespace dStruct
+{
+
+
+}
---
+++ kurbyko.aleksey/T2/DataStruct.hpp
@@ -0,0 +1,16 @@
+#ifndef DATASTRUCT_H
+#define DATASTRUCT_H
+
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <iomanip>
+#include <string>
+
+namespace dStruct
+{
+
+
+}
+
+#endif
