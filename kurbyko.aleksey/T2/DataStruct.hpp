0ade04468e4e4889f2fa29d43ebc1e3beb14bfdd
lechat222 <kurbyko2006@gmail.com>
2025-09-28 17:54:35 +0300

    Add structures for parsing

--- kurbyko.aleksey/T2/DataStruct.hpp
+++ kurbyko.aleksey/T2/DataStruct.hpp
@@ -6,6 +6,9 @@
 #include <iterator>
 #include <iomanip>
 #include <string>
+#include <algorithm>
+#include <cctype>
+#include <bitset>
 
 namespace dStruct
 {
@@ -16,10 +19,38 @@ namespace dStruct
     std::string key3;
   };
 
+  struct DelimiterIO
+  {
+    char exp;
+  };
 
+  struct ULLBinary
+  {
+    unsigned long long& value;
+  };
+
+  struct CharI
+  {
+    char& value;
+  };
+
+  struct StrKeyI
+  {
+    std::string& value;
+  };
+
+  struct StringIO
+  {
+    const char* expected;
+  };
+
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
+  std::istream& operator>>(std::istream& in, ULLBinary&& key);
+  std::istream& operator>>(std::istream& in, CharI&& key);
+  std::istream& operator>>(std::istream& in, StrKeyI&& key);
+  std::istream& operator>>(std::istream& in, const StringIO&& dest);
   std::istream& operator>>(std::istream& in, DataStruct& dest);
   std::ostream& operator<<(std::ostream& out, const DataStruct& dest);
-
 
 }
 
