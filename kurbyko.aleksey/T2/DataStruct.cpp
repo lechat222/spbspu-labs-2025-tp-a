26953581b8030c19558a4efc15d71365b9998e9b
lechat222 <kurbyko2006@gmail.com>
2025-09-28 21:59:03 +0300

    bimbimbambam

--- kurbyko.aleksey/T2/DataStruct.cpp
+++ kurbyko.aleksey/T2/DataStruct.cpp
@@ -1,8 +1,10 @@
 #include "DataStruct.hpp"
+#include <algorithm>
 
 namespace dStruct
 {
-  std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry)
       return in;
@@ -20,7 +22,7 @@ namespace dStruct
     return "0" + binString.erase(0, binString.find('1'));
   }
 
-  std::ostream& operator<<(std::ostream& out, const DataStruct& value) {
+  std::ostream &operator<<(std::ostream &out, const DataStruct &value) {
     std::ostream::sentry sentry(out);
     if (!sentry) {
       return out;
@@ -28,13 +30,13 @@ namespace dStruct
     iofmtguard fmtguard(out);
 
     out << "(:key1 0b" << convertIntoBinNumber(value.key1);
-    out << ":key2 " << value.key2;
+    out << ":key2 \'" << value.key2 << "\'";
     out << ":key3 \"" << value.key3 << "\":)";
 
     return out;
   }
 
-  std::istream& operator>>(std::istream& in, ULLBinary&& key)
+  std::istream& operator>>(std::istream &in, ULLBinary &&key)
   {
     std::istream::sentry sentry(in);
     if (!sentry)
@@ -106,7 +108,7 @@ namespace dStruct
     return in;
   }
 
-  std::istream& operator>>(std::istream& in, DataStruct& value) {
+  std::istream& operator>>(std::istream &in, DataStruct &value) {
     std::istream::sentry sentry(in);
     if (!sentry) {
       return in;
@@ -117,7 +119,7 @@ namespace dStruct
     size_t KEY_AMOUNT = 3;
     unsigned int keyI{};
 
-    in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
+    in >> DelimiterIO{ '(' } >> DelimiterIO{':'};
 
     while (in && KEY_AMOUNT > 0)
     {
@@ -126,17 +128,17 @@ namespace dStruct
       {
       case 1:
       {
-        in >> ULLBinary{ value.key1 };
+        in >> ULLBinary{value.key1};
         break;
       }
       case 2:
       {
-        in >> CharI{ value.key2 };
+        in >> CharI{value.key2};
         break;
       }
       case 3:
       {
-        in >> StrKeyI{ value.key3 };
+        in >> StrKeyI{value.key3};
         break;
       }
       default:
@@ -145,16 +147,16 @@ namespace dStruct
       }
       }
       --KEY_AMOUNT;
-      in >> DelimiterIO{ ':' };
+      in >> DelimiterIO{':'};
     }
 
     in >> DelimiterIO{ ')' };
     return in;
   }
 
-  iofmtguard::iofmtguard(std::basic_ios<char>& s)
-    : s_(s), width_(s.width()), fill_(s.fill()), precision_(s.precision()),
-    fmt_(s.flags()) {}
+  iofmtguard::iofmtguard(std::basic_ios<char> &s)
+      : s_(s), width_(s.width()), fill_(s.fill()), precision_(s.precision()),
+        fmt_(s.flags()) {}
 
   iofmtguard::~iofmtguard() {
     s_.width(width_);
