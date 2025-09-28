93ff88343fa9f1de643d1e8f32a105258e84846d
lechat222 <kurbyko2006@gmail.com>
2025-09-28 18:10:39 +0300

    Add a class to save and restore the state of input and output streams

--- kurbyko.aleksey/T2/DataStruct.hpp
+++ kurbyko.aleksey/T2/DataStruct.hpp
@@ -44,6 +44,19 @@ namespace dStruct
     const char* expected;
   };
 
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios<char>& s);
+    ~iofmtguard();
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize width_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios<char>::fmtflags fmt_;
+  };
+
   std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
   std::istream& operator>>(std::istream& in, ULLBinary&& key);
   std::istream& operator>>(std::istream& in, CharI&& key);
